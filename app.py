15a283b3ba44f423c586413199f449ede1b755d9
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2023-09-22 15:23:23 +0200

    omgwtfrotfl

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def index():  # put application's code here
     return 'Hello World!'
+
+@app.route('/about')
+def about():
+    return "Autorem tego programu jest Mapet który lubi pierogi i bigos"
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True,port=80)
+
+#60. Dodaj do aplikacji ekran /show_players który
+#będzie wyświetlał jakiś tekst. Przetestuj działanie.
